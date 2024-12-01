e44bd17ac43814670dfcd5171b10eb8751bae4d9
Ștefan-Iulian Alecu <96354838+stalecu@users.noreply.github.com>
2024-12-01 17:04:17 +0200

    fix(2024-01): fix fuck up

--- 2024/day01/perl/day01.pl
+++ 2024/day01/perl/day01.pl
@@ -1,26 +1,26 @@
 use v5.40;
-use List::Util 'reduce', 'zip';
+use List::Util 'reduce';
 
 my @left;
 my @right;
 
-open DATA, q|inputs/input| or die "Cannot open file: $!";
+open DATA, q|../inputs/input|;
 while (<DATA>) {
     chomp;
-    my @line = split /\s+/;
+    my @line = split /\s+/, $_;
     push @left,  $line[0];
     push @right, $line[1];
 }
 
-my @sorted = sort { $a->[0] <=> $b->[0] } map { [ $_->[0], $_->[1] ] } zip(\@left, \@right);
+@left  = sort { $a <=> $b } @left;
+@right = sort { $a <=> $b } @right;
 
-my $part_1 = reduce { $a + abs($b->[0] - $b->[1]) } @sorted;
+my $part_1 = reduce { $a + abs( $left[$b] - $right[$b] ) } 0 .. $#left;
 
 my %right_counts = map { $_ => 0 } @right;
 $right_counts{$_}++ for @right;
-my $part_2 = reduce { $a + $b * ($right_counts{$b} // 0) } 0, @left;
+
+my $part_2 = reduce { $a + $b * ( $right_counts{$b} // 0 ) } 0, @left;
 
 say "Part 1: $part_1";
 say "Part 2: $part_2";
-
-close DATA;
